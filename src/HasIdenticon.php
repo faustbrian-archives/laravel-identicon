43542c3fa08866a74c00efdaeb160ed53e61e4c2
Brian Faust <hello@brianfaust.de>
2016-12-02 12:44:33 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/HasIdenticon.php
+++ src/HasIdenticon.php
@@ -1,9 +1,16 @@
 <?php
 
+/*
+ * This file is part of Laravel Identicon.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Identicon;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use function BrianFaust\Identicon\identicon;
 
 trait HasIdenticon
@@ -11,12 +18,12 @@ trait HasIdenticon
     /** @var array */
     public $identicon = [
         'from' => 'email',
-        'to' => 'avatar'
+        'to'   => 'avatar',
     ];
 
     public static function bootHasIdenticon()
     {
-        static::created(function($model){
+        static::created(function ($model) {
             $identicon = identicon($model->{$model->identicon['from']});
 
             $model->{$model->identicon['to']} = $identicon;
