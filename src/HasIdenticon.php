63783028b8b4ca3aa3db1d20ca39d8fcc2e4837f
Brian Faust <faustbrian@users.noreply.github.com>
2017-02-22 14:19:32 +0200

    Use creating even

--- src/HasIdenticon.php
+++ src/HasIdenticon.php
@@ -11,15 +11,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Identicon.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\Identicon;
 
 use function BrianFaust\Identicon\identicon;
@@ -34,8 +25,9 @@ trait HasIdenticon
 
     public static function bootHasIdenticon(): void
     {
-        static::created(function ($model) {
-            $model->generateIdenticon();
+        static::creating(function ($model) {
+            $identicon = identicon($model->{$model->identicon['from']});
+            $model->{$model->identicon['to']} = $identicon;
         });
     }
 
